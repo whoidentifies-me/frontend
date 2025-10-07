54fb12d6235ebaedcae50099a4b3c4b1be3323fa
Tobias Muehlberger <tobias@muehlberger.dev>
2025-10-07 21:34:57 +0200

    Improved linting and settings for browser compatibility checking
    Signed-off-by: Tobias Muehlberger <tobias@muehlberger.dev>

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,9 +2,11 @@ import js from "@eslint/js";
 import solid from "eslint-plugin-solid/configs/typescript";
 import * as tsParser from "@typescript-eslint/parser";
 import globals from "globals";
+import compat from "eslint-plugin-compat";
 
 export default [
   js.configs.recommended,
+  compat.configs["flat/recommended"],
   {
     files: ["**/*.{ts,tsx}"],
     ...solid,
